ce4535fea8be34e7fff699f0ae638f5211c86467
emna-abdelmoula <emnaabdelmoula66@gmail.com>
2024-07-26 13:06:54 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { TripFormComponent } from './trip-form/trip-form.component';
+
+const routes: Routes = [
+
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'landingpage',
+    component:LandingpageComponent
+  },
+  { path: 'trip-form',
+    component: TripFormComponent 
+  }
+  ,
+  { path: 'register',
+    component: RegisterComponent
+  },
+  
+  {
+      path:'',
+      pathMatch:'full',
+      redirectTo:'register'
+  },
+
+  {
+    path:'login',
+    component:LoginComponent
+  },
+
+  {
+    path:'**',//ken ma9itch 7ata path meli moujoudin
+    redirectTo:'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
